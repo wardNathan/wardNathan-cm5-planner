339dcaaa1ce350349733567034f35a3842ce4039
wardNathan <xuserx98@gmail.com>
2022-11-28 22:02:11 -0700

    Commiting the bulk of changes made in code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # wardNathan-cm5-planner
+
+## Description
+
+This project is a planner that allows you to save notes for your work day to the local storage. It will help you keep track of your tasks by indicating the present hour, and showing you which hours are coming up and which have passed. 
+
+## Installation
+
+N/A
+
+## Usage
+
+A user can click within the colored boxes to enter text and then must press the blue save button to add it to local storage. 
+
+## Credits
+
+N/A
+
+## License
+
+MIT License
+
+## Link and Screenshot
+
+![alt text](./assets/images/cm5ss.jpg)
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var timeDisplayEl = $('#currentDay');
 var input_textarea9 = document.querySelector('.content-input9');
 var save_button9 = document.querySelector('.save-button9');
@@ -21,12 +18,33 @@ var save_button4 = document.querySelector('.save-button4');
 var input_textarea5 = document.querySelector('.content-input5');
 var save_button5 = document.querySelector('.save-button5');
 
+var hour = dayjs().hour();
+
 function displayTime() {
-    var rightNow = dayjs().format('MMM DD, YYYY');
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm');
     timeDisplayEl.text(rightNow);
-    console.log
   }
-displayTime();
+
+  var hour = dayjs().hour();
+
+function background () {
+  $('.backgroundColor').each(function () {
+    var timeOfDay = parseInt($(this).attr('id'));
+    hour = parseInt(hour);
+  if(hour > timeOfDay){
+    $(this).addClass('past');
+  }
+  else if(hour < timeOfDay){
+    $(this).addClass('future');
+  }
+  else {
+    $(this).addClass('present');
+  }
+  });
+}
+
+displayTime()
+background()
 
 save_button9.addEventListener('click', updateOutput9);
 
@@ -126,25 +144,3 @@ function updateOutput5() {
 
   input_textarea5.textContent = input_textarea5.value;
 }
-
-/*
-$(function () {
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-  });
-  */
